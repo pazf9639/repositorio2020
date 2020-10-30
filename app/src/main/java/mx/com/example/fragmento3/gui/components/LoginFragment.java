514acf37f41fcf09cc52ed0a5abd2fbc4a1c090e
pazf9639 <70867876+pazf9639@users.noreply.github.com>
2020-10-30 02:13:23 -0600

    v1.1.2

--- app/src/main/java/mx/com/example/fragmento3/gui/components/LoginFragment.java
+++ app/src/main/java/mx/com/example/fragmento3/gui/components/LoginFragment.java
@@ -1,5 +1,6 @@
 package mx.com.example.fragmento3.gui.components;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,15 +8,40 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
-import mx.com.example.fragmento3.R;
+import mx.com.example.fragmento3.databinding.FragmentLoginBinding;
+import mx.com.example.fragmento3.gui.MainActivity;
+import mx.com.example.fragmento3.gui.TopJuegos;
 
 public class LoginFragment extends Fragment {
-
+private FragmentLoginBinding binding;
+private View view;
+private Context context;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        configGlobals();
+        configView(inflater,container);
+        configListeners();
+       return view;
+    }
+
+    private void configListeners() {
+        binding.nextButon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((NavigationHost)MainActivity.GLOBALS.get("app")).navigateTo(new TopJuegos(),false);
+            }
+        });
+    }
+
+    private void configView(LayoutInflater inflater, ViewGroup container) {
+        binding=FragmentLoginBinding.inflate(inflater,container,false);
+        view=binding.getRoot();
+        context=container.getContext();
+    }
+
+    private void configGlobals() {
+        MainActivity.GLOBALS.put("Loading fragment",this);
     }
 }
